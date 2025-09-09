10d8d8013070207b33eab7bc9852af131232f86a
Рысаев Дамир <freepad@ya.ru>
2025-09-09 13:43:12 +0300

    fix test

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -20,6 +20,6 @@ describe('farewell function', () => {
 
   it('should handle empty name gracefully', () => {
     const result = farewell('')
-    expect(result).toBe('Goodbye, ! 1')
+    expect(result).toBe('Goodbye, !')
   })
 })
